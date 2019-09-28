310702667c03256b30cb8465cd6ea3b167dfd0c5
Roman Pelikh <pelikh.roman@gmail.com>
2019-09-28 16:59:02 -0300

    add jackson lib, fetched data

--- src/testapi/TestApi.java
+++ src/testapi/TestApi.java
@@ -5,14 +5,16 @@
  */
 package testapi;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.Map;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
 
 
 /**
@@ -27,54 +29,107 @@ public class TestApi {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Call();
-    }
-
-    public static void Call() {
         try {
-            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+            Call2();
+                
+        } catch (Exception e) {
+        }
+        
+        
+    }
+    
+    
+     public static void Call2() throws MalformedURLException, ProtocolException{
+          try {
+            URL url = new URL("https://api.edamam.com/api/nutrition-data?app_id=f7c18e2c&app_key=4d954df472a36b23bdd06f81aeff9cc6&ingr=1%20large%20apple");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
 
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
+//            if (conn.getResponseCode() != 200) {
+//                throw new RuntimeException("Failed : HTTP error code : "
+//                        + conn.getResponseCode());
+//            }
 
             BufferedReader response = new BufferedReader(new InputStreamReader(
                     (conn.getInputStream())));
             
-             Object obj = new JSONParser().parse(response); 
-            JSONArray ja = (JSONArray)obj;
             
-            Iterator it = ja.iterator();
+            ObjectMapper objectMapper = new ObjectMapper();
             
-             Iterator<Map.Entry> itr1;
-            //ITERATE THROU ALL ALL OBJECTS IN ARRAY
-            while(it.hasNext()){
-                itr1 = ((Map) it.next()).entrySet().iterator();
-                
-                
-                //ITERATE THROUGH PROPERTIES IN ARRAY
-                while(itr1.hasNext()){
-                    Map.Entry pair = itr1.next();
-                    System.out.println(pair.getKey() + " ========= " + pair.getValue());
-                }
-            }
            
-        
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = response.readLine()) != null) {
-                System.out.println(output);
-            }
-
+            
+              Map<String, Object> jsonMap = objectMapper.readValue(response,
+    new TypeReference<Map<String,Object>>(){});
+              
+              
+              Map<String, Object> nutrients = new HashMap<>();
+              
+              jsonMap.entrySet().forEach(entry -> {
+              
+                  nutrients.put(entry.getKey(), entry.getValue()); 
+              
+              });
+              
+              
+              
+                        
+            
             conn.disconnect();
         } catch (Exception e) {
         }
+         
+         
+         
+     }
+    
+
+    public static void Call() {
+//        try {
+//            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            
+//            conn.setRequestMethod("GET");
+//            conn.setRequestProperty("Accept", "application/json");
+//
+//            if (conn.getResponseCode() != 200) {
+//                throw new RuntimeException("Failed : HTTP error code : "
+//                        + conn.getResponseCode());
+//            }
+//
+//            BufferedReader response = new BufferedReader(new InputStreamReader(
+//                    (conn.getInputStream())));
+//            
+//             Object obj = new JSONParser().parse(response); 
+//            JSONArray ja = (JSONArray)obj;
+//            
+//            Iterator it = ja.iterator();
+//            
+//             Iterator<Map.Entry> itr1;
+//            //ITERATE THROU ALL ALL OBJECTS IN ARRAY
+//            while(it.hasNext()){
+//                itr1 = ((Map) it.next()).entrySet().iterator();
+//                
+//                
+//                //ITERATE THROUGH PROPERTIES IN ARRAY
+//                while(itr1.hasNext()){
+//                    Map.Entry pair = itr1.next();
+//                    System.out.println(pair.getKey() + " ========= " + pair.getValue());
+//                }
+//            }
+//           
+//        
+//
+//            String output;
+//            System.out.println("Output from Server .... \n");
+//            while ((output = response.readLine()) != null) {
+//                System.out.println(output);
+//            }
+//
+//            conn.disconnect();
+//        } catch (Exception e) {
+       // }
 
     }
 
